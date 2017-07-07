3cb8a47572945e1178c4f62d53681d4294452941
ChinatsuKOBAYASHI <andc03-d88@ezweb.ne.jp>
2017-07-07 19:24:51 +0900

    aaa

---
+++ login_success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta http-equiv="refresh" content="3;URL=top.html">
+  <title>ログイン画面</title>
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="stylesheet" type="text/css" href="decorate.css">
+ </head>
+
+ <body>
+ <div class="container">
+ <div class="content">
+   <h2>ログイン成功</h2>
+ </div>
+ </div>
+ </body>
+</html>
